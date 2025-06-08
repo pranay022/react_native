39f92061f8a4ea8327c1b30421978ef8d2e217dc
pranay <pranay4228@gmail.com>
2025-06-08 23:07:00 +0530

    add project 4

---
+++ project04/src/App.tsx
@@ -0,0 +1,123 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+
+import DiceOne from '../assests/One.png'
+import DiceTwo from '../assests/Two.png'
+import DiceThree from '../assests/Three.png'
+import DiceFour from '../assests/Four.png'
+import DiceFive from '../assests/Five.png'
+import DiceSix from '../assests/Six.png'
+
+type DiceProps = PropsWithChildren<{
+  imageUrl:ImageSourcePropType
+}>
+
+
+// Optional configuration
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+ReactNativeHapticFeedback.trigger("rigid");
+
+
+const Dice =({imageUrl}: DiceProps): JSX.Element => {
+  
+  return(
+    <View>
+      <Image style={styles.diceImage}  source={imageUrl} />
+    </View>
+  )
+}
+
+
+function App(): React.JSX.Element {
+  const [diceImage, setDiceImage] =  useState<ImageSourcePropType>(DiceOne) 
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random() * 6 ) + 1;
+    switch(randomNumber){
+      case 1:
+        setDiceImage(DiceOne)
+        break;
+      case 2:
+        setDiceImage(DiceTwo)
+        break;
+      case 3:
+        setDiceImage(DiceThree)
+        break;
+      case 4:
+        setDiceImage(DiceFour)
+        break;
+      case 5:
+        setDiceImage(DiceFive)
+        break;
+      case 6:
+        setDiceImage(DiceSix)
+        break;
+      default :
+        setDiceImage(DiceOne)
+        break;
+    }
+    ReactNativeHapticFeedback.trigger("impactLight", options);
+  }
+  return (
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable
+      onPress={rollDiceOnTap}
+      >
+        <Text
+        style={styles.rollDiceBtnText}
+        >
+          Roll the dice
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    marginTop:60
+  },
+});
+
+export default App;
